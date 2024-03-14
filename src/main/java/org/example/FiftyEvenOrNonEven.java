c29b3845fe6b021a4bcd52e143c40ad541c61e5a
MykolaTereshchukTR-12 <nikter387@gmail.com>
2024-03-14 22:30:15 +0200

    Commit 2. Trying to create a class that was supposed to create, initialize, and populate arrays. Unsuccessfully.

---
+++ src/main/java/org/example/FiftyEvenOrNonEven.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class FiftyEvenOrNonEven {
+    private int[] evenNumber = new int[50];
+    private int[] nonEvenNumber = new int[50];
+
+    public int[] getEvenNumber() {
+        return evenNumber;
+    }
+
+    public void setEvenNumber(int[] evenNumber) {
+        this.evenNumber = evenNumber;
+    }
+
+    public int[] getNonEvenNumber() {
+        return nonEvenNumber;
+    }
+
+    public void setNonEvenNumber(int[] nonEvenNumber) {
+        this.nonEvenNumber = nonEvenNumber;
+    }
+}
