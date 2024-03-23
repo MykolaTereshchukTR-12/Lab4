bc69a3d1ab3b2d3fb28680c24273b976cc3070e9
Mykola <nikter387@gmail.com>
2024-03-23 15:46:25 +0200

    Commit 16. Modernization of the code with the possibility of setting a custom array for task 2.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,8 +11,13 @@ public class Main {
         System.out.println(Arrays.toString(fiftyEvenOrNonEven.getEvenNumber()));
         System.out.println(Arrays.toString(fiftyEvenOrNonEven.getNonEvenNumber()));
 
-        //Change this line for more universal array
-        int[] array = {2, 17, 13, 6, 22, 31, 45, 66, 100, -18};
+        System.out.println("Well, let's create a new array!");
+        System.out.println("What size will your array be?");
+        int[] array = new int[scanner.nextInt()];
+        System.out.println("Enter the integer elements of the array alternately and with a space)");
+        for(int i = 0; i != array.length; i++){
+            array[i] = scanner.nextInt();
+        }
 
         ArrayOperation arrayOperation = new ArrayOperation();
         arrayOperation.setMineArray(array);
